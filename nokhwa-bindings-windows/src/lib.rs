f6321f15bca3b188b8705a43b1fd873ec4cac2d1
Owain Cleaver <owain@localhost>
2025-04-10 15:32:21 +0100

    Fix for https://github.com/l1npengtul/nokhwa/issues/192.

--- nokhwa-bindings-windows/src/lib.rs
+++ nokhwa-bindings-windows/src/lib.rs
@@ -265,6 +265,10 @@ pub mod wmf {
 
         let mut device_list = vec![];
 
+        if count == 0 {
+            return Ok(device_list);
+        }
+
         unsafe { from_raw_parts(unused_mf_activate.assume_init(), count as usize) }
             .iter()
             .for_each(|pointer| {
